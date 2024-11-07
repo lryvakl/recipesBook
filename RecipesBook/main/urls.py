97cbd04b43eb5f249066d7f6847f987e1d05dd45
lryvakl <leraruvak2006@gmail.com>
2024-11-07 22:39:57 +0200

    доданий пошук на головній сторінці

--- RecipesBook/main/urls.py
+++ RecipesBook/main/urls.py
@@ -6,12 +6,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='mainPage'),
-    path('about-us',views.recipes_view, name='about-us'),
-    path('login',views.login),
-    path('register',views.register),
-    path('changePass',views.changePass),
-    path('profile',views.profile),
-    path('addRecipe', views.addRecipe, name='addRecipe'),
+    path('about-us/',views.recipes_view, name='about-us'),
+    path('login/',views.login, name='login'),
+    path('register/',views.register, name='register'),
+    path('changePass/',views.changePass, name='changePass'),
+    path('profile/',views.profile, name='profile'),
+    path('addRecipe/', views.addRecipe, name='addRecipe'),
     path('add_recipe/', views.add_recipe, name='add_recipe'),
-    path('recipes/', views.recipes_view, name='recipes')
+    path('recipes/', views.recipes_view, name='recipes'),
+    path('about-us', views.search_recipes, name='search'),
+    path('search/', views.search_ingredients, name='search_ingredients'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
